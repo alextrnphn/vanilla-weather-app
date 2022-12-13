b569e5e18285908f35df4319e56d992f235c39be
alextrnphn <alextranphan@gmail.com>
2022-12-13 12:23:58 -0800

    add searchform, style btn

--- src/index.js
+++ src/index.js
@@ -38,9 +38,17 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
+function search(city) {
+  let apiKey = "6f578b96aa9505bcce148ac22cb85794";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "6f578b96aa9505bcce148ac22cb85794";
-let city = "San Jose";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("paris");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
